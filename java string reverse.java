d89ac21bfd8d3a11e468b1e691039e8ffb677d36
Yuvarajan S <yuvarajanyuvarajan16@gmail.com>
2025-05-19 23:16:45 +0530

    Create java string reverse.java

---
+++ java string reverse.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String reversed = new StringBuilder(A).reverse().toString();
+        System.out.println(A.equals(reversed)?"Yes":"No");
+        
+    }
+}
+
+
+
